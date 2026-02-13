77da512b773533542e78d7e98978d32101618bd5
Sabari-2107 <sabari2107amp@gmail.com>
2026-02-13 10:35:43 +0530

    data

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
